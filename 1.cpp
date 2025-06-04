016118e050057170224edb1852b2d4fdd11fcc13
hanzalahwaheed <hanzalah.w@gmail.com>
2025-06-05 01:48:30 +0530

    First Commit

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+  string s;
+  cin >> s;
+  cout << s;
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+
+  solve();
+  return 0;
+}
